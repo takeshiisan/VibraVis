885e576daceca56a32556f7911fb3a76508023e1
takeshiisan <takeshisan29@gmail.com>
2026-08-28 17:51:32 +0800

    included wire.h, drv2605.h, vl53l7cx.h, audio.h - then added unsigned long int for last motor trigger and polltime - then added unsigned 16 bit integer for tracking previous distance/time per sensor for computation of approach speed

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
-// testing a new branch in git 
-// any comment
+/* 
+VibraVis: Specialized Haptic Feedback Eyeglasses
+--------------------------------------------------
 
+
+
+*/
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_DRV2605.h>
+#include <Adafruit_VL53L7CX.h>
+#include "Audio.h"
 #include "config.h"
+
+Adafruit_VL53L7CX vl53l7cx;
+VL53L7CX_ResultsData results;
+
+Adafruuit_DRV2605 motors[MOTOR_COUNT];
+Audio audio;
+
+unsigned long lastMotorTrigger[MOTOR_COUNT] = {0};
+unsigned long lastPollTime = 0;
+ 
+// Tracks previous distance/time per sensor, used to compute approach speed
+uint16_t previousDistance[SENSOR_COUNT]   = {0};
+unsigned long previousReadTime[SENSOR_COUNT] = {0};
