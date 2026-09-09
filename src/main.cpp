62558e061e87e70ed837ed12550a642cf3628726
takeshiisan <takeshisan29@gmail.com>
2026-09-09 14:38:52 +0800

    cleaned dead code

--- src/main.cpp
+++ src/main.cpp
@@ -190,23 +190,6 @@ uint8_t sensorToMotorMask(int sensorIndex) {
   }
 }
 
-/*
-EFFECT ID MAPPING: For simplicity, we can use the same effect ID for all motors. 
-(WILL NOT BE USED FOR NOW).
-*/
-
-// void triggerMotor(uint8_t motorMask, uint8_t effectId) {
-//   for(int i = 0; i < MOTOR_COUNT; i++) {
-//     if(motorMask & (1 << i)) {
-//       selectMuxChannel(motorMuxMappings[i].muxAddress, motorMuxMappings[i].channel);
-//       motors[i].setWaveform(0, effectId); // set effect
-//       motors[i].setWaveform(1, 0); // end of sequence
-//       motors[i].go();
-//     }
-//   }
-// }
-
-
 // THEORETICALLY, we could use the DRV2605L's RTP mode to set a continuous vibration intensity based on distance.
 
 uint8_t distanceToAmplitude(uint16_t distanceMm) {
@@ -251,10 +234,6 @@ void processObstacles() {
   updateMotorIntensities(mask, amplitude);
 
   Serial.printf("ALERT! Sensor %d | Dist: %d mm | Speed: %.1f mm/s | Amplitude: %d\n", priorityIndex, distances[priorityIndex], speeds[priorityIndex], amplitude);
-    // triggerMotor(mask, effect);
-    // lastMotorTrigger = millis();
-    // Serial.printf("ALERT! Sensor %d | Dist: %d mm | Speed: %.1f mm/s\n", priorityIndex, distances[priorityIndex], speeds[priorityIndex]);
-  //}
 }
 
 // Initializes the MAX17043 fuel gauge and sets the low battery alert threshold.
